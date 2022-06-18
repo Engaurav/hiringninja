260e79061a417b346bf30b0c1a8865d8fea87890
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-18 12:46:13 +0530

    Added Static file for ejs using scss

--- index.js
+++ index.js
@@ -5,16 +5,47 @@ const express = require("express");
 const app = express();
 const expressLayout = require('express-ejs-layouts');  //for creating partial in express
 const mongoose = require("./config/mongoose"); // Calling Mongoose Config File for Database
+const sassMiddleware = require('node-sass-middleware')
+
+
+
+
+
+//for scss middleware
+app.use(sassMiddleware({
+    src:  './assets/scss',
+    dest: './assets/css',
+    debug: false,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
 
 // this is to use our post data of form
 app.use(express.urlencoded());
 
+
+
+// connect static directory
+app.use(express.static('./assets'));
+
+
+
 // calling partial middleware
 app.use(expressLayout);
+
+
+
+
+//extract style and script from webpages into the layout
+app.set('layout extractStyles', true)
+app.set('layout extractScripts', true)
+
 
 //setting up our view engine for ejs
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
 
 // calling all the routes using use middleware
 app.use("/", require("./routes/index"));
