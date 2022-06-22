a559f631620797e84576274acb5648b75f127f7b
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-23 01:39:44 +0530

    Navbar Media width CSS

--- assets/javascripts/header.js
+++ assets/javascripts/header.js
@@ -8,10 +8,34 @@ function showProfileDropdown(){
     let profileDropdown = document.getElementById("nav-profile-dropdown");
     
     if(showProfile){
-        profileDropdown.style.height = "0px";
+        profileDropdown.style.display = "none";
         showProfile = false
     }else{
-        profileDropdown.style.height = "80px";
+        profileDropdown.style.display = "block";
+        showProfile = true;
+    }
+
+}
+
+// function to handle sidebar button
+let showSide = false;
+async function showSideBar(){
+    let sidebar = document.getElementById("sidebar");
+    let navLinks = document.getElementById("nav-links");
+    let navProfile = document.getElementById("nav-profile");
+
+
+
+    if(showProfile){
+        sidebar.style.display = "none";
+        navProfile.style.display = "none";
+        navLinks.style.display = "flex";
+
+        showProfile = false
+    }else{
+        navProfile.style.display = "block";
+        sidebar.style.display = "block";
+        navLinks.style.display = "none !important";
         showProfile = true;
     }
 
